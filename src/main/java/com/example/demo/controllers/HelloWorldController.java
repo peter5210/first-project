5cd0c8e2c552a979b57099f42fc183d0f1c1dfd5
Peter Boudreau <Peter.Boudreau@LibertyMutual.com>
2017-08-10 20:10:34 -0400

    refactor to create new math controller, fixed adder get issue

--- src/main/java/com/example/demo/controllers/HelloWorldController.java
+++ src/main/java/com/example/demo/controllers/HelloWorldController.java
@@ -1,58 +1,46 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.example.demo.model.Adder;
 import com.example.demo.model.Whisperer;
 import com.example.demo.model.Yeller;
 
 @Controller
-@RequestMapping({"/", "/HelloWorld"})
+@RequestMapping({ "/", "/HelloWorld" })
 public class HelloWorldController {
-	
+
 	private String title;
-	
+
 	public HelloWorldController() {
 		title = "Hello Java and Spring!";
 	}
-	
+
 	@GetMapping("")
 	public String index() {
 		return "helloworld/index";
 	}
-	
+
 	@GetMapping("message")
-	public ModelAndView message(@RequestParam(required=false, defaultValue="«silence»") String shoutMessage) {
-			Yeller loud = new Yeller(shoutMessage);
+	public ModelAndView message(@RequestParam(required = false, defaultValue = "«silence»") String shoutMessage) {
+		Yeller loud = new Yeller(shoutMessage);
 		String result = loud.makeUpperCase();
 		ModelAndView mv = new ModelAndView("helloworld/message");
 		mv.addObject("title", title);
 		mv.addObject("message", result);
 		return mv;
 	}
-		
+
 	@GetMapping("whisper")
-	public ModelAndView whisper(@RequestParam(required=false, defaultValue="«shhhhh»") String silenceMessage) {
-			Whisperer quiet = new Whisperer(silenceMessage);
+	public ModelAndView whisper(@RequestParam(required = false, defaultValue = "«shhhhh»") String silenceMessage) {
+		Whisperer quiet = new Whisperer(silenceMessage);
 		String result = quiet.makeLowerCase();
 		ModelAndView mv = new ModelAndView("helloworld/whisper");
 		mv.addObject("title", title);
 		mv.addObject("whisper", result);
-		return mv; 	
+		return mv;
 	}
-	 
-	@PostMapping("adder")
-		public String addTwoNumbers(@RequestParam(name="left") int first, @RequestParam(name="right") double second, Model model) {
-		Adder adder = new Adder(first, second);
-		double result = adder.calculate();
-		model.addAttribute("sum", result);
-		return "helloworld/sum-result";
-	}
-	
+
 }
